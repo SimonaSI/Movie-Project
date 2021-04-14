e6eab70e20a5dc21ef7e404afe18410b3ae74fa9
SimonaSI <75039328+SimonaSI@users.noreply.github.com>
2021-04-14 19:13:33 +0300

    Stagiu Practica Roweb

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public string DirectedBy { get; set; }
+
+        public string DistributedBy { get; set; }
+
+        public string RunningTime { get; set; }
+
+        public string Language { get; set; }
+
+        
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movies.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movies.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        DirectedBy = "Rob Reiner",
+                        DistributedBy = "Columbia Pictures",
+                        RunningTime = "96 mins",
+                        Language = "English"
+
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        DirectedBy = "Ivan Reitman",
+                        DistributedBy = "Columbia Pictures",
+                        RunningTime = "105 mins",
+                        Language = "English"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        DirectedBy = "Ivan Reitman",
+                        DistributedBy = "Columbia Pictures",
+                        RunningTime = "108 mins",
+                        Language = "English"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        DirectedBy = "Howard Hawks",
+                        DistributedBy = "Warner Bros",
+                        RunningTime = "141 mins",
+                        Language = "English and Spanish"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
